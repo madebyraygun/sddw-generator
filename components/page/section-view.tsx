5f5e0ffeb1e42b33dd7b75de636e941abe8954d4
David Robbins <davidrobbins@davids-mbp-16.lan>
2021-05-19 20:20:43 -0700

    setting up page sections

---
+++ components/page/section-view.tsx
@@ -0,0 +1,11 @@
+import styles from './section-view.module.scss';
+
+import { CustomProps } from '../types/props';
+
+const PageSectionView: FC<CustomProps> = ({ children, className, dataName }) => (
+  <div element='page-section-view' className={`${className ?? ''} ${styles['page-section-view']}`} {...dataName}>
+    { children }
+  </div>
+);
+
+export default PageSectionView;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,48 @@
-import Page from '../components/app/page';
 import Intro from '../components/intro';
 import EditorControls from '../components/editor/editor-controls';
+import PageSectionView from '../components/page/section-view';
 // import Feed from '../components/feed';
 
-const Index: FC = () => (
-  <Page slug="index">
-    <Intro />
-    <EditorControls />
-  </Page>
+
+interface Reference {
+  el?: HTMLElement,
+  intro?: HTMLElement,
+  editor?: HTMLElement,
+}
+class PageIndexElement extends HTMLElement {
+
+  static selector = 'page-index';
+
+  ref: Reference = {};
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.ref.el = this;
+    console.log(this.ref.el.querySelector('[data-section-intro]'));
+  }
+
+}
+
+// connect markup to javascript class -------------------------------------- //
+
+if (!window.customElements.get(PageIndexElement.selector)) {
+  window.customElements.define(PageIndexElement.selector, PageIndexElement);
+}
+
+// JSX template ------------------------------------------------------------ //
+
+const PageIndex: FC = () => (
+  <div element={ PageIndexElement.selector } slug="index">
+    <PageSectionView dataName={{ 'data-section-intro': '', 'data-active': '' }}>
+      <Intro />
+    </PageSectionView>
+    <PageSectionView dataName={{ 'data-section-editor': '' }}>
+      <EditorControls />
+    </PageSectionView>
+  </div>
 );
 
-export default Index;
+export default PageIndex;
